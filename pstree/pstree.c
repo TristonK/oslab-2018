0037e6755d608252adc86aab7b3232bb74110474
tracer-nju <tracer@nju.edu.cn>
2019-03-06 00:08:15 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8cdedcfde7854208b015bb38822d79d7            Boot ID: f50a70c1ad1f4fb3a010789a801b30bc   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-29-generic       Architecture: x86-64  00:08:15 up  1:14,  3 users,  load average: 0.07, 0.32, 0.57

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -5,7 +5,8 @@ int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < argc; i++) {
     assert(argv[i]); // always true
-		switch(argv[i]){
+		int op=argv[i];
+		switch(op){
 			case '-p':
 							printf("print pid\n");
 							break;
