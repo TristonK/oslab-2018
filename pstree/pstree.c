1979051c752cdcdf62120dd41613eb0bb9425b6e
tracer-nju <tracer@nju.edu.cn>
2019-03-09 19:09:30 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8cdedcfde7854208b015bb38822d79d7            Boot ID: a82a9ceff9264f9b8ccca07e4bbf60c1   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-29-generic       Architecture: x86-64  19:09:31 up 44 min,  1 user,  load average: 0.84, 0.41, 0.38

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 //test
 int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < argc; i++) {
     assert(argv[i]); // always true
-		int op=(int)argv[i];
-		if(argv[i]=="-p" || argv[i] == "--show-pids")
+		if(!strcmp(argv[i],"-p") || !strcmp(argv[i],"--show-pids"))
 			printf("print pids\n");
-		else if(argv[i] == "-n" || argv[i] == "--numeric-sort")
+		else if(!strcmp(argv[i],"-n") || !strcmp(argv[i],"--numeric-sort"))
 			printf("sort\n");
-		else if(argv[i] == "-V" || argv[i] == "--version")
+		else if(!strcmp(argv[i],"-V") || !strcmp(argv[i],"--version"))
 		  printf("pstree (kuangsl) 1.0\n Copyright (C) 2019-2019 what a sad lab\n");
     //printf("argv[%d] = %s\n", i, argv[i]);
   }
