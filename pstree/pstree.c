180d52c7dd75efd41ab10a6da70299ecbd45109a
tracer-nju <tracer@nju.edu.cn>
2019-03-09 20:19:53 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8cdedcfde7854208b015bb38822d79d7            Boot ID: a82a9ceff9264f9b8ccca07e4bbf60c1   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-29-generic       Architecture: x86-64  20:19:54 up  1:55,  1 user,  load average: 0.12, 0.18, 0.10

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-//test
+#include <dirent.h>
+
+//variables
+
+
+
+void read_proc(){
+	DIR *dirptr = NULL;
+	struct dirent *entry;
+	dirptr = opendir("/proc");
+	assert(dirptr == NULL);
+	while(entry = readdir(dirptr)){
+		if(entry.d_type == DT_DIR)
+			printf("yes\n");		
+	}
+}
+
+
 int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < argc; i++) {
@@ -11,9 +28,11 @@ int main(int argc, char *argv[]) {
 		else if(!strcmp(argv[i],"-n") || !strcmp(argv[i],"--numeric-sort"))
 			printf("sort\n");
 		else if(!strcmp(argv[i],"-V") || !strcmp(argv[i],"--version"))
-		  printf("pstree (kuangsl) 1.0\n Copyright (C) 2019-2019 what a sad lab\n");
-    //printf("argv[%d] = %s\n", i, argv[i]);
+		  printf("pstree (kuangsl) 1.0\Copyright (C) 2019-2019 what a sad lab\n");
+		else
+		 	printf("pstree\n");
   }
+	read_proc();
   assert(!argv[argc]); // always true
   return 0;
 }
