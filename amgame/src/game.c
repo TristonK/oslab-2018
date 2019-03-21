31822aef32f736277c4966ec85a144f5c7f2fe43
tracer-nju <tracer@nju.edu.cn>
2019-03-21 22:52:00 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: f995f6cdbf62426d969bc40184cb7890   Operating System: Ubuntu 16.04.2 LTS             Kernel: Linux 4.8.0-36-generic       Architecture: x86-64  22:52:00 up  6:39,  3 users,  load average: 0.52, 0.58, 0.50

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -95,15 +95,15 @@ int main() {//go through
 
 int read_key1() {
   _DEV_INPUT_KBD_t event = { .keycode = _KEY_NONE };
-  /*#define KEYNAME(key) \
+  #define KEYNAME(key) \
     [_KEY_##key] = #key,
   static const char *key_names[] = {
     _KEYS(KEYNAME)
-  };*/
+  };
   _io_read(_DEV_INPUT, _DEVREG_INPUT_KBD, &event, sizeof(event));
   if (event.keycode != _KEY_NONE && event.keydown) {
     //puts("Key pressed: ");
-    //puts(key_names[event.keycode]);
+    puts(key_names[event.keycode]);
     //puts("\n");
     if(event.keycode==_KEY_S)
       return 1;
