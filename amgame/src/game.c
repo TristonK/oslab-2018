fcf1bef9af61b08dbda12418d3ebc602a45224b0
tracer-nju <tracer@nju.edu.cn>
2019-03-21 17:36:43 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: f995f6cdbf62426d969bc40184cb7890   Operating System: Ubuntu 16.04.2 LTS             Kernel: Linux 4.8.0-36-generic       Architecture: x86-64  17:36:43 up  1:24,  1 user,  load average: 0.18, 0.10, 0.09

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,21 +1,25 @@
 #include <game.h>
+#include <klib.h>
 
 void init_screen();
 void splash();
-void read_key();
+void read_key1();
+//void get_white_block(); 
+//int white_block[100];
 
-int main() {
+int main() {//dont touch white blocks with max socre:100
   // Operating system is a C program
   _ioe_init();
   init_screen();
   splash();
+ // get_white_block();
   while (1) {
-    read_key();
+    read_key1();
   }
   return 0;
 }
 
-void read_key() {
+void read_key1() {
   _DEV_INPUT_KBD_t event = { .keycode = _KEY_NONE };
   #define KEYNAME(key) \
     [_KEY_##key] = #key,
@@ -24,10 +28,20 @@ void read_key() {
   };
   _io_read(_DEV_INPUT, _DEVREG_INPUT_KBD, &event, sizeof(event));
   if (event.keycode != _KEY_NONE && event.keydown) {
-    puts("Key pressed: ");
+    //puts("Key pressed: ");
     puts(key_names[event.keycode]);
     puts("\n");
+   /* if(key_names[event.keycode]=='1')
+      return 1;
+    else if(key_names[event.keycode]=='2')
+      return 2;
+    else if(key_names[event.keycode]=='3')
+      return 3;
+    else if
+      return 0;*/
   }
+  /*else
+    return 0;*/
 }
 
 int w, h;
@@ -39,7 +53,7 @@ void init_screen() {
   h = info.height;
 }
 
-void draw_rect(int x, int y, int w, int h, uint32_t color) {
+void draw_rect1(int x, int y, int w, int h, uint32_t color) {
   uint32_t pixels[w * h]; // WARNING: allocated on stack
   _DEV_VIDEO_FBCTL_t event = {
     .x = x, .y = y, .w = w, .h = h, .sync = 1,
@@ -55,8 +69,14 @@ void splash() {
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
       if ((x & 1) ^ (y & 1)) {
-        draw_rect(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
+        draw_rect1(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
       }
     }
   }
 }
+
+/*void get_white_block(){
+  for(int i=0;i<100;i++){
+    white_block[i]=(rnad()%3)+1;
+  }
+}*/
