48852f1b2f7cd3cec88083dbef10114799f64d3c
171240501 Kuang ShuLei <923950043@qq.com>
2019-04-14 23:05:32 +0800

    change stdio.c

--- abstract-machine/libs/klib/src/stdio.c
+++ abstract-machine/libs/klib/src/stdio.c
@@ -5,7 +5,10 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  char pbuf[1024];
+  //char* test = "T";
+	//_putc(*test);
+	//_putc('\n');
+	char pbuf[1024];
   va_list ap;
   int i;
   va_start(ap,fmt);
@@ -47,6 +50,23 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 			  while(num){
 				  tmp[i]=numtrans[num%10];
 				  num/=10;
+				  i++;
+			  }
+			  while(i){
+				  i--;
+				  *str++=tmp[i];
+			  }
+			  break;
+			case 'x':
+				num=va_arg(ap,int);
+			  int i=0;char tmp[20];
+			  if(num==0){
+				  *str++='0';
+				  break;
+			  }
+			  while(num){
+				  tmp[i]=numtrans[num%16];
+				  num/=16;
 				  i++;
 			  }
 			  while(i){
