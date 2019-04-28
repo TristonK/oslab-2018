7c2b5f774577946663d3facc9595a493e8f03c02
tracer-nju <tracer@nju.edu.cn>
2019-04-28 21:26:50 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 1e8dc5d78fe54d159b4cbb1d9fb6c543   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.8.0-36-generic       Architecture: x86-64  21:26:50 up  7:24,  1 user,  load average: 0.11, 0.20, 0.27

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -62,7 +62,8 @@ void phase_st(char phase[1024]){
 void paint_line(){
     //printf("\x1b[2J");
     //system("clear");
-    printf("\x1B[2J\x1B[0;0f");
+    //printf("\x1B[2J\x1B[0;0f");
+    printf("\033[2J");
     double sum_time=0;
     for(int i=0;i<proc_cnt;i++){
         sum_time+=p[i].proc_time;
