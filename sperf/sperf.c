182b2d6c1b8cb78fe668db9ee6b3b2f538fe7152
tracer-nju <tracer@nju.edu.cn>
2019-04-28 16:13:41 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 1e8dc5d78fe54d159b4cbb1d9fb6c543   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.8.0-36-generic       Architecture: x86-64  16:13:41 up  2:11,  1 user,  load average: 0.54, 0.58, 0.49

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 #include <string.h>
-//int pipe(int fildes[2]);
+int fildes[2];
 
 int main(int argc, char *argv[]) {
-  printf("argc is %d\n",argc);
-  printf("%s\n",argv[0]);
-  /*pid_t pid = fork();
+  char *input_op[50]={"strace","-T"};
+  char *environ[2] = {"PATH=/bin",NULL};
+  for(int i=1;i<argc;i++){
+    input_op[i+1]=argv[i];
+  }
+  if(pipe(fildes)!=0){
+    printf("Fail to create pipe\n");
+    assert(0);
+  }
+  pid_t pid = fork();
   if(!pid){
     printf("hi is son\n");
-    execve("/use/bin/strace",["strace","-T"]);
+    close(fildes[0]);
+    dup2(fildes[1],2);
+    execve("/use/bin/strace",input_op,environ);
+    printf("we shouldnt go here]\n");
   } else{
+    close(fildes[1]);
+    dup2(fildes[0],0);
+    char read_info[50000];
+    fgets(read_info, sizeof(read_info),0);
+    printf("%s\n",read_info);
     printf("father is here\n");
-  }*/
+  }
 
 
 
