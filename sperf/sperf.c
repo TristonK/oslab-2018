d60ecbe42b22e474a8c153991de59f7b28c74d7f
tracer-nju <tracer@nju.edu.cn>
2019-04-28 19:36:35 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 1e8dc5d78fe54d159b4cbb1d9fb6c543   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.8.0-36-generic       Architecture: x86-64  19:36:35 up  5:34,  1 user,  load average: 0.60, 0.49, 0.65

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -40,6 +40,8 @@ void phase_st(char phase[1024]){
     }
     temp_time[time_cnt]='\0';
     double temp_timeval = atof(temp_time);
+    if(temp_timeval<1e-6)
+        return;
     int have_it = 0;
     for(int j=0;j<proc_cnt;j++){
         if(strcmp(temp_name,p[j].proc_name)==0){
@@ -91,7 +93,7 @@ int main(int argc, char *argv[]) {
   }
   pid_t pid = fork();
   if(!pid){
-        printf("hi is son\n");
+        //printf("hi is son\n");
         close(fildes[0]);
         dup2(fildes[1],2);
         //printf("\x1b[44mA\n");
@@ -102,7 +104,7 @@ int main(int argc, char *argv[]) {
   } else{
         close(fildes[1]);
         dup2(fildes[0],0);
-        printf("father is here\n");
+        //printf("father is here\n");
         char read_info[1024];
         while(fgets(read_info, sizeof(read_info),stdin)!=NULL){
             phase_st(read_info);
