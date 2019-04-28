65189e3eb91942e6c37ef28bb8862eee164e4b75
tracer-nju <tracer@nju.edu.cn>
2019-04-28 20:28:34 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 1e8dc5d78fe54d159b4cbb1d9fb6c543   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.8.0-36-generic       Architecture: x86-64  20:28:34 up  6:26,  1 user,  load average: 0.62, 0.46, 0.46

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -83,7 +83,7 @@ void paint_line(){
         if(p[i].proc_rate){
             switch (judge_color){
                 case 0:
-                    printf("\x1b[42m%s:  %d%%\n",p[i].proc_name,p[i].proc_rate);
+                    printf("\x1b[42m%s:  %d%%",p[i].proc_name,p[i].proc_rate);
                     printf("\x1b[0m\n");
                     break;
                 case 1:
