e2397478e8cf16cbe7dfcfc7fe699aebd5d3b5a1
tracer-nju <tracer@nju.edu.cn>
2019-04-28 15:49:58 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 1e8dc5d78fe54d159b4cbb1d9fb6c543   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.8.0-36-generic       Architecture: x86-64  15:49:58 up  1:47,  1 user,  load average: 0.05, 0.26, 0.39

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+//int pipe(int fildes[2]);
+
 int main(int argc, char *argv[]) {
+  printf("argc is %d\n",argc);
+  printf("%s\n",argv[0]);
+  /*pid_t pid = fork();
+  if(!pid){
+    printf("hi is son\n");
+    execve("/use/bin/strace",["strace","-T"]);
+  } else{
+    printf("father is here\n");
+  }*/
+
+
+
+
   return 0;
 }
