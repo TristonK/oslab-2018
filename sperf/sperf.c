0dcc2c318486a9ab4a4c85b1ade6ed5011b7dfe1
tracer-nju <tracer@nju.edu.cn>
2019-04-28 19:30:02 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 1e8dc5d78fe54d159b4cbb1d9fb6c543   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.8.0-36-generic       Architecture: x86-64  19:30:02 up  5:27,  1 user,  load average: 0.20, 0.48, 0.76

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -17,6 +17,7 @@ void phase_st(char phase[1024]){
     char temp_name[32];
     char temp_time[32];
     int i=0;
+    printf("%s\n",phase);
     while (phase[i]!='('){
         temp_name[i]=phase[i];
         i++;
@@ -86,14 +87,14 @@ int main(int argc, char *argv[]) {
   }
   pid_t pid = fork();
   if(!pid){
-    printf("hi is son\n");
-    close(fildes[0]);
-    dup2(fildes[1],2);
-    //printf("\x1b[44mA\n");
-    //printf("\x1b[32mHello, World\\n")
-    execve("/usr/bin/strace",input_op,environ);
-    printf("You shouldn't go here\n");
-    assert(0);
+        printf("hi is son\n");
+        close(fildes[0]);
+        dup2(fildes[1],2);
+        //printf("\x1b[44mA\n");
+        //printf("\x1b[32mHello, World\\n")
+        execve("/usr/bin/strace",input_op,environ);
+        printf("You shouldn't go here\n");
+        assert(0);
   } else{
         close(fildes[1]);
         dup2(fildes[0],0);
