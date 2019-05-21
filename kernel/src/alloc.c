55d2ed66d8855199205a981ad46bc1c947732652
tracer-nju <tracer@nju.edu.cn>
2019-05-21 21:00:35 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: b6a2b663e0cf41d9a209d578c01d6002   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  21:00:35 up 36 min,  1 user,  load average: 1.39, 0.92, 0.94

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -76,7 +76,7 @@ void show_alloc(){
   if(runlist.size){
     kblock *pr = runlist.head -> next;
     while(1){
-      printf("begin at %d and end at %d and size is %d\n",(int)pr->begin_addr,(int)pr->end_addr,(int)pr->size);
+      printf("begin at %I64u and end at %I64u and size is %I64u\n",pr->begin_addr, pr->end_addr, pr->size);
       if(pr->next==NULL)
         break;
       pr = pr->next;
