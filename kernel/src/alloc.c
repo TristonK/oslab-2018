b49333f3f9d53f8dccf88fe9576bebfc2f1f4bc1
tracer-nju <tracer@nju.edu.cn>
2019-05-23 20:48:47 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 888d555cca1c4f3b996d404467c76f13   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  20:48:47 up  4:49,  1 user,  load average: 0.92, 0.85, 0.69

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -75,6 +75,7 @@ LOCKDEF(print);
 
 void show_alloc(){
   print_lock();
+  printf("|----------------------------|\n");
   printf("now i will show runlist with size %d\n",runlist.size);
   if(runlist.size){
     kblock *pr = runlist.head -> next;
@@ -96,6 +97,7 @@ void show_alloc(){
       pr2 = pr2->next;
     }
   }
+  printf("|----------------------------|\n");
   print_unlock();
 }
 
@@ -146,7 +148,7 @@ static int find_free_block(){
 static void block_cut(kblock *blockc,uintptr_t need_size){
     //spin_lock(&print_lk);
     print_lock();
-    printf("you used memory from %d to %d from cpu %d\n",blockc->begin_addr,blockc->begin_addr+need_size,_cpu()+1);
+    printf("you used memory from %d to %d from cpu %d\n",blockc->begin_addr,blockc->begin_addr+need_size,(_cpu()+1));
     //spin_unlock(&print_lk);
     print_unlock();
     if(blockc->size==need_size){
