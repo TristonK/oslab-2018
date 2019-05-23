30204cb933c1b3fc3f21d26429d513dee5117263
tracer-nju <tracer@nju.edu.cn>
2019-05-23 17:30:02 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 888d555cca1c4f3b996d404467c76f13   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  17:30:02 up  1:30,  1 user,  load average: 1.03, 0.89, 0.65

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -76,7 +76,7 @@ void show_alloc(){
   if(runlist.size){
     kblock *pr = runlist.head -> next;
     while(1){
-      printf("begin at 0x%x and end at 0x%x and size is 0x%x\n",&pr->begin_addr, &pr->end_addr, pr->size);
+      printf("begin at 0x%x and end at 0x%x and size is 0x%x\n",&pr->begin_addr, &pr->end_addr, &pr->size);
       if(pr->next==NULL)
         break;
       pr = pr->next;
@@ -86,7 +86,7 @@ void show_alloc(){
   if(freelist.size){
     kblock *pr2 = freelist.head ->next;
     while(1){
-      printf("begin at 0x%x and end at 0x%x and size is 0x%x\n",&pr2->begin_addr,&pr2->end_addr,pr2->size);
+      printf("begin at 0x%x and end at 0x%x and size is 0x%x\n",&pr2->begin_addr,&pr2->end_addr,&pr2->size);
       if(pr2->next==NULL)
         break;
       pr2 = pr2->next;
