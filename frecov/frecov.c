953cec2891d587f232e67a82c905decf15959703
tracer-nju <tracer@nju.edu.cn>
2019-06-01 15:05:02 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: abde0cd2f5664c369559d878ea06d0c6   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  15:05:02 up  1:47,  1 user,  load average: 0.38, 0.44, 0.66

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -9,21 +9,52 @@
 
 int fd;
 char *buf;
+struct BPB{
+  //.... have more but we dont need it
+  unsigned short bytsPerSec; //0xb
+  char SecPerClus;   //0xd
+  unsigned short ReservedSec; //0xe
+  char FatNum; //0x10
+  unsigned short RootEnt;//should be 0
+  unsigned short SmallSec;// be 0;
+  char MediaDescri;
+  unsigned short SecPerFat; //0x16
+  unsigned short SecPerTrack; //0x18
+  unsigned short NumHeads;//0x1a
+  unsigned int HiddenSec;
+  unsigned int LargeSec;
+  unsigned int SecPerFat;
+  //.... have more but we dont need it
+};
+
+struct BPB bpb;
+
+void get_info(){
+  memcpy(&bpb,buf+0xb,sizeof(bpb));
+  printf("each sec has byte %d\n",bpb.bytsPerSec);
+  printf("each Cluster has %d sec\n",bpb.SecPerClus);
+  printf("you reserve %d sec\n",bpb.ReservedSec);
+  printf("you have %d Fat\n",bpb.FatNum);
+}
+
+
+
+
+
 
 int main(int argc, char *argv[]) {
+  //*******read file img
   fd = open(argv[1],O_RDONLY);
-  printf("You will recover %s\n",argv[1]);
+  //printf("You will recover %s\n",argv[1]);
   if(fd<0){
     printf("ERROR: YOU entered an bad addr!\n");
     assert(0);
   }
   buf = (char*) mmap(NULL,1<<27,PROT_READ,MAP_SHARED,fd,0);
-  printf("in 0x0b is %d\n",buf[0x0b]);
-  printf("in 0x0b+1 is %d\n",buf[0x0c]);
-  printf("in 0xd is %d\n",buf[0x0d]);
-  printf("in 0xe is %d\n",buf[0x0e]);
-  printf("in 0xe+1 is %d\n",buf[0x0f]);
-  printf("in 0x10 is %d\n",buf[0x10]);
+  //*******recovery
+  get_info();
+  
+  //*******Stop work
   munmap((void*)buf,1<<27);
   close(fd);
   return 0;
