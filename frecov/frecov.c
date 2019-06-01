1d904892fcac4acf6421960a90982edbf8835929
tracer-nju <tracer@nju.edu.cn>
2019-06-01 13:55:53 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: abde0cd2f5664c369559d878ea06d0c6   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  13:55:53 up 38 min,  1 user,  load average: 1.86, 1.27, 0.96

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>    
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <assert.h>
+
+int fd;
+char *buf;
+
 int main(int argc, char *argv[]) {
+  fd = open(argv[1],O_RDONLY);
+  if(fd<0){
+    printf("ERROR: YOU entered an bad addr!\n");
+    assert(0);
+  }
+  buf = (char*) mmap(NULL,1<<27,PROT_READ,MAP_SHARED,fd,0);
+  printf("in 0x0b is %c\n",&buf[0x0b]);
+  printf("in 0x0b+1 is %c\n",&buf[0x0c]);
+  printf("in 0xd is %c\n",&buf[0x0d]);
+  printf("in 0xe is %c\n",&buf[0x0e]);
+  printf("in 0xe+1 is %c\n",&buf[0x0f]);
+  printf("in 0x10 is %c\n",&buf[0x10]);
+  munmap((void*)buf,1<<27);
+  close(fd);
   return 0;
 }
