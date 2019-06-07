1578e3d16dbc00ee2b2624b54d63436663a1bc47
tracer-nju <tracer@nju.edu.cn>
2019-06-07 19:52:33 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 88ebcf31f66f4216a25e249281eab323   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  19:52:33 up 11 min,  1 user,  load average: 2.13, 2.51, 1.61

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -4,16 +4,7 @@
 spinlock_t ostrap;
 handle* hde; 
 
-void echo_task(void *name) {
-  device_t *tty = dev_lookup(name);
-  while (1) {
-    char line[128], text[128];
-    sprintf(text, "(%s) $ ", name); tty_write(tty, text);
-    int nread = tty->ops->read(tty, 0, line, sizeof(line));
-    line[nread - 1] = '\0';
-    sprintf(text, "Echo: %s.\n", line); tty_write(tty, text);
-  }
-}
+extern void echo_task(void *name) 
 
 static void os_init() {
   pmm->init();
