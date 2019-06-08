41a224296f8fb97857a4eecd6510fca92fce6de1
tracer-nju <tracer@nju.edu.cn>
2019-06-08 17:13:09 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: dcc75f79990247c88d7e1867732177cb   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  17:13:09 up  4:02,  1 user,  load average: 0.72, 0.68, 0.76

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -51,8 +51,8 @@ static void os_run() {
 }
 
 static _Context *os_trap(_Event ev, _Context *context) {
-  printf("trap begin\n");
   kmt->spin_lock(&ostrap);
+  printf("trap begin\n");
   _Context *ret = NULL;
   handle *h = hde;
   //if(hde==NULL)
