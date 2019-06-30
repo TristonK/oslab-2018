f4ee390b3bb316321dd44f3777a72577f59d5a92
tracer-nju <tracer@nju.edu.cn>
2019-06-30 15:01:48 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: a95678e2dd0a403e80911607309f04c9   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-52-generic       Architecture: x86-64  15:01:48 up  5:28,  1 user,  load average: 0.78, 0.74, 0.76

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -58,7 +58,7 @@ static void hello() {
 static void os_run() {
   hello();
   _intr_write(1);
-  printf("maxcpu is %d\n",_ncpu());
+  //printf("maxcpu is %d\n",_ncpu());
   while (1) {
     _yield();
   }
