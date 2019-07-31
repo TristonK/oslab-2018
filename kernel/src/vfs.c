3a767d67435ab1062d2b94387e5b29f8cfb7ea14
tracer-nju <tracer@nju.edu.cn>
2019-07-31 18:59:17 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 4c2a7ceccc52473b8c8e4fc02c2a3457   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-55-generic       Architecture: x86-64  18:59:17 up 10:51,  1 user,  load average: 1.80, 1.65, 1.01

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -64,7 +64,6 @@ void vfs_init (){
     vfs->mkdir("proc");
 	//vfs->mount("/proc",&procfs,"proc");
 	vfs->mkdir("/dev/tty1");
-    printf("shit\n");
 	vfs->mkdir("/dev/tty2");
 	vfs->mkdir("/dev/tty3");
 	vfs->mkdir("/dev/tty4");
