167bd4e907b73734edbec82a07239c4b63d3bef0
tracer-nju <tracer@nju.edu.cn>
2019-07-31 16:10:39 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 4c2a7ceccc52473b8c8e4fc02c2a3457   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-55-generic       Architecture: x86-64  16:10:39 up  8:02,  1 user,  load average: 1.21, 1.23, 1.25

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -95,8 +95,7 @@ int vfs_mkdir (const char *path){
     //printf("is is %d and %d\n",i,lens-1);
     //printf("%s\n",(path+1));
     while(strncmp((path+i),"/",1)){
-        //printf("i is %d\n");
-        //printf("path[i] is %s",path+i);
+        printf("i is %d\n",i);
         i = i-1;
     }
     printf("%d  \n",i);
