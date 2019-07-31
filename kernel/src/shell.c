8d8e8709822a79caaf9a4f38add6ddb73b7afd1e
tracer-nju <tracer@nju.edu.cn>
2019-07-31 19:06:37 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 4c2a7ceccc52473b8c8e4fc02c2a3457   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-55-generic       Architecture: x86-64  19:06:37 up 10:58,  1 user,  load average: 0.95, 1.15, 1.02

--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -35,7 +35,7 @@ void path_translation(const char* path){
 
 void shell_thread(void* ttyid) {
     //output form: "buf: current_path$ stdin"  
-    int tty_id = *ttyid;
+    int tty_id = (int)*ttyid;
     char buf[32];
     //printf("66666666666666666\n");
     printf("this iddd is %d\n",tty_id);
