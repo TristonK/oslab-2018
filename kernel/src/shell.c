b19c7f002b6b9c5c8653f594b54a58eba3c847e2
tracer-nju <tracer@nju.edu.cn>
2019-07-31 21:08:56 +0800

    Static hostname: kuangsl          Icon name: computer-laptop            Chassis: laptop         Machine ID: 808615223fbc4b5cad3034e9fa6a719d            Boot ID: 4c2a7ceccc52473b8c8e4fc02c2a3457   Operating System: Ubuntu 16.04.6 LTS             Kernel: Linux 4.15.0-55-generic       Architecture: x86-64  21:08:56 up 13:00,  1 user,  load average: 0.78, 1.16, 1.20

--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -22,6 +22,7 @@ void path_translation(const char* path){
             strcpy(newpath,"/");
         }else{
             printf("%d ss\n",i);
+            printf("%s\n",current_path);
             strncpy(newpath,current_path,i);
         }
         printf("%s\n",newpath);
